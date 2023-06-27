a611ae6cf2c418ad4b37dc0ae9d5cbcd9487d64d
natejmaster@gmail.com <natejmaster@gmail.com>
2023-06-27 08:35:36 -0500

    Added pseudo-code as comments on javascript sheet

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//For reference sake, this line and below is where my code is written.
+//When the button is clicked, user is presented with a series of prompts
+  //When prompted for password criteria, I select which criteria to include
+  //When prompted for the length of the password, then user chooses a length between 8 characters and 128 characters
+  //When asked for character types to include in the password, then I confirm whether or not to include certain character types
+    //include or exclude lowercase
+    //include or exclude uppercase
+    //include or exclude numeric
+    //include or exclude special characters.
+  //This type prompt should be validated to ensure at least one character type was selected
+  //Password that matches select criteria is generated
+  //When password is generated, it is either printed as an alert or on the page
+//This is where my code stops
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
